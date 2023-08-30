8c86fce19da32e434007cfb8e9259a12e6f0aa98
YadavYashvant <yashvantyadav855@gmail.com>
2023-08-30 18:40:46 +0530

    created data class

--- app/src/main/java/com/example/text_recognition/MainActivity.kt
+++ app/src/main/java/com/example/text_recognition/MainActivity.kt
@@ -1,15 +1,35 @@
 package com.example.text_recognition
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.text_recognition.ui.theme.TextRecognitionTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,6 +43,39 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
 
+                    var imageUri: Any? by remember { mutableStateOf(R.drawable.img_defualt) }
+                    val photoPicker = rememberLauncherForActivityResult(
+                        contract = ActivityResultContracts.PickVisualMedia()
+                    ) {
+                        if (it!=null) {
+                            Log.d("PhotoPicker","Selected URI: $it")
+                            imageUri = it
+                        }
+                        else{
+                            Log.d("PhotoPicker","No media Selected")
+                        }
+                    }
+
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        AsyncImage(
+                            modifier = Modifier
+                                .size(300.dp)
+                                .clickable {
+                                    photoPicker.launch(
+                                        PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                                    )
+                                },
+                            model = ImageRequest.Builder(LocalContext.current).data(imageUri) ,
+                            contentDescription = "Default Image",
+                            contentScale = ContentScale.Crop
+                            )
+
+                        Spacer(modifier = Modifier.height(30.dp))
+                    }
                 }
             }
         }
---
+++ app/src/main/java/com/example/text_recognition/MainScreenState.kt
@@ -0,0 +1,6 @@
+package com.example.text_recognition
+
+data class MainScreenState(
+    val extractedText: String = "Not yet..",
+    val isButtonEnabled: Boolean = true
+)
